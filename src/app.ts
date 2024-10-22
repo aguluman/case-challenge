95181e4b1cb5004869f0c5546ebf25aae0a66068
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2024-10-22 15:02:39 +0100

    Refactor: Remove unused code for initializing TypeORM Data Source

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import 'reflect-metadata';
 import './container'; // Import the container configuration
-import { HabariDataSource } from './config/orm.config';
 import express, { Request, Response, NextFunction } from 'express';
 import { errorHandling } from './utils/error-handling.utils';
 import { HttpException } from './utils/exception/http.exception';
@@ -10,13 +9,6 @@ import specs from './swagger.config';
 
 
 const app = express();
-
-// Initialize TypeORM Data Source
-HabariDataSource.initialize()
-  .then(() => {
-    console.log('Database connected successfully');
-  })
-  .catch((error) => console.log(error));
 
 app.use(express.json());
 
